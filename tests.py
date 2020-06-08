8011ea21d779f99270f91a50e0ff7b60c8cb2b00
Thomas Hu <tomhu1096@gmail.com>
2020-06-08 18:05:20 -0400

    test it

--- tests.py
+++ tests.py
@@ -7,5 +7,8 @@ class TestMethods(unittest.TestCase):
     def test_add(self):
         self.assertEqual(awesome.smile(), ":)")
 
+    def test_money(self):
+        self.assertEqual(awesome.money(), "$")
+
 if __name__ == '__main__':
     unittest.main()
